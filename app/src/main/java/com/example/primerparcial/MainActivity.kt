b507b7a4ad41e7d326c39100572edd1f2254ff59
Lucas-89 <mansillalucas1212@gmail.com>
2024-05-07 19:14:08 -0300

    Configuracion basica de las paginas

--- app/src/main/java/com/example/primerparcial/MainActivity.kt
+++ app/src/main/java/com/example/primerparcial/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PrimerParcialTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    MainPage(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -30,18 +30,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PrimerParcialTheme {
-        Greeting("Android")
-    }
-}
--- app/src/main/java/com/example/primerparcial/MainPage.kt
+++ app/src/main/java/com/example/primerparcial/MainPage.kt
@@ -1,4 +1,23 @@
 package com.example.primerparcial
 
-class MainPage {
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.primerparcial.ui.theme.PrimerParcialTheme
+
+@Composable
+fun MainPage(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Pagina de Ingreso",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainPagePreview() {
+    PrimerParcialTheme {
+        MainPage("Android")
+    }
 }
